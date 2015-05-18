71cec4ee5a935dd99c2b1e2585e587a778e6ab56
yasenia <pineislet@outlook.com>
2015-05-18 17:48:14 +0800

    Save thesis.

--- src/main/java/com/pineislet/graduation/ga/GeneticEngine.java
+++ src/main/java/com/pineislet/graduation/ga/GeneticEngine.java
@@ -52,138 +52,138 @@ public class GeneticEngine<T extends Individual> {
         populationHistory.add(originalPopulation);
     }
 
-    /**
-     *  进化
-     * */
-    public void evolution() {
-        /**
-         *  选择
-         * */
-        List<T> selectedPopulation = select(currentPopulation);
-
-        /**
-         *  交叉
-         * */
-        List<T> crossoverPopulation = crossover(selectedPopulation);
-
-        /**
-         *  变异，记录当前种群
-         * */
-        currentPopulation = mutate(crossoverPopulation);
-
-        /**
-         *  记录种群历史
-         * */
-        populationHistory.add(currentPopulation);
-    }
-
+/**
+ *  进化
+ * */
+public void evolution() {
     /**
      *  选择
      * */
-    @SuppressWarnings("unchecked")
-    private List<T> select(List<T> population) {
-        List<T> resultPopulation = new ArrayList<>();
-        // 计算适应度
-        double[] fitnessArray = new double[populationSize];
-        for (int i = 0; i < populationSize; i++) {
-            fitnessArray[i] = population.get(i).calcFitness();
-        }
-        // 计算适应度均值
-        double sum = 0;
-        for (double fitness : fitnessArray) {
-            sum += fitness;
-        }
-        double avg = sum / populationSize;
-        // 计算适应度标准差
-        double sqSum = 0;
-        for (double fitness : fitnessArray) {
-            sqSum += (fitness - avg) * (fitness - avg);
-        }
-        double sd = Math.sqrt(sqSum / populationSize);
-        // 适应度尺度变换
-        double fMin = avg - sd;
-        for (int j = 0; j < populationSize; j++) {
-            fitnessArray[j] = fitnessArray[j] > fMin ? fitnessArray[j] - fMin : 0;
-        }
-
-        // 选择个体
-        for (int i = 0; i < populationSize; i++) {
-            int index = Utils.roulette(fitnessArray);
-            Individual individual = population.get(index);
-            resultPopulation.add((T) individual.createIndividual(individual.getGenome()));
-        }
-        return resultPopulation;
-    }
+    List<T> selectedPopulation = select(currentPopulation);
 
     /**
      *  交叉
      * */
-    @SuppressWarnings("unchecked")
-    private List<T> crossover(List<T> population) {
-        List<T> resultPopulation = new ArrayList<>();
-        // 随机打乱
-        Collections.shuffle(population);
-        // 两两配对
-        for (int i = 0; i < populationSize / 2; i++) {
-            Individual individual1 = population.get(i);
-            Individual individual2 = population.get(populationSize - 1 - i);
-
-            // 获取两者基因组
-            List<boolean[]> genome1 = individual1.getGenome();
-            List<boolean[]> genome2 = individual2.getGenome();
-
-            List<boolean[]> crossGenome1 = new ArrayList<>();
-            List<boolean[]> crossGenome2 = new ArrayList<>();
-            assert genome1.size() == genome2.size();
-            // 基因交叉
-            for (int j = 0; j < genome1.size(); j++) {
-                boolean[] gene1 = genome1.get(j);
-                boolean[] gene2 = genome2.get(j);
-                // 随机选取交叉点
-                assert gene1.length == gene2.length;
-                int geneLength = gene1.length;
-                int index = (int) (Math.random() * geneLength);
-                // 基因交叉互换
-                boolean[] crossGene1 = new boolean[geneLength];
-                boolean[] crossGene2 = new boolean[geneLength];
-                for (int k = 0; k < geneLength; k++) {
-                    if (k < index) {
-                        crossGene1[k] = gene1[k];
-                        crossGene2[k] = gene2[k];
-                    }
-                    else {
-                        crossGene1[k] = gene2[k];
-                        crossGene2[k] = gene1[k];
-                    }
-                }
-                crossGenome1.add(crossGene1);
-                crossGenome2.add(crossGene2);
-            }
-
-            resultPopulation.add((T) individual1.createIndividual(crossGenome1));
-            resultPopulation.add((T) individual2.createIndividual(crossGenome2));
-        }
-        return resultPopulation;
-    }
+    List<T> crossoverPopulation = crossover(selectedPopulation);
 
     /**
-     *  变异
+     *  变异，记录当前种群
      * */
-    @SuppressWarnings("unchecked")
-    private List<T> mutate(List<T> currentPopulation) {
-        return currentPopulation.stream().map(
-                individual -> (T) individual.createIndividual(individual.getGenome().stream().map(gene -> {
-                    boolean[] mutateGene = new boolean[gene.length];
-                    if (gene.length > 0) {
-                        mutateGene[0] = gene[0];
-                        for (int i = 1; i < gene.length; i++) {
-                            mutateGene[i] = Math.random() < P_MUTATE ? (!gene[i]) : gene[i];
-                        }
-                    }
-                    return mutateGene;
-                }).collect(Collectors.toList()))
-        ).collect(Collectors.toList());
+    currentPopulation = mutate(crossoverPopulation);
+
+    /**
+     *  记录种群历史
+     * */
+    populationHistory.add(currentPopulation);
+}
+
+/**
+ *  选择
+ * */
+@SuppressWarnings("unchecked")
+private List<T> select(List<T> population) {
+    List<T> resultPopulation = new ArrayList<>();
+    // 计算适应度
+    double[] fitnessArray = new double[populationSize];
+    for (int i = 0; i < populationSize; i++) {
+        fitnessArray[i] = population.get(i).calcFitness();
     }
+    // 计算适应度均值
+    double sum = 0;
+    for (double fitness : fitnessArray) {
+        sum += fitness;
+    }
+    double avg = sum / populationSize;
+    // 计算适应度标准差
+    double sqSum = 0;
+    for (double fitness : fitnessArray) {
+        sqSum += (fitness - avg) * (fitness - avg);
+    }
+    double sd = Math.sqrt(sqSum / populationSize);
+    // 适应度尺度变换
+    double fMin = avg - sd;
+    for (int j = 0; j < populationSize; j++) {
+        fitnessArray[j] = fitnessArray[j] > fMin ? fitnessArray[j] - fMin : 0;
+    }
+
+    // 选择个体
+    for (int i = 0; i < populationSize; i++) {
+        int index = Utils.roulette(fitnessArray);
+        Individual individual = population.get(index);
+        resultPopulation.add((T) individual.createIndividual(individual.getGenome()));
+    }
+    return resultPopulation;
+}
+
+/**
+ *  交叉
+ * */
+@SuppressWarnings("unchecked")
+private List<T> crossover(List<T> population) {
+    List<T> resultPopulation = new ArrayList<>();
+    // 随机打乱
+    Collections.shuffle(population);
+    // 两两配对
+    for (int i = 0; i < populationSize / 2; i++) {
+        Individual individual1 = population.get(i);
+        Individual individual2 = population.get(populationSize - 1 - i);
+
+        // 获取两者基因组
+        List<boolean[]> genome1 = individual1.getGenome();
+        List<boolean[]> genome2 = individual2.getGenome();
+
+        List<boolean[]> crossGenome1 = new ArrayList<>();
+        List<boolean[]> crossGenome2 = new ArrayList<>();
+        assert genome1.size() == genome2.size();
+        // 基因交叉
+        for (int j = 0; j < genome1.size(); j++) {
+            boolean[] gene1 = genome1.get(j);
+            boolean[] gene2 = genome2.get(j);
+            // 随机选取交叉点
+            assert gene1.length == gene2.length;
+            int geneLength = gene1.length;
+            int index = (int) (Math.random() * geneLength);
+            // 基因交叉互换
+            boolean[] crossGene1 = new boolean[geneLength];
+            boolean[] crossGene2 = new boolean[geneLength];
+            for (int k = 0; k < geneLength; k++) {
+                if (k < index) {
+                    crossGene1[k] = gene1[k];
+                    crossGene2[k] = gene2[k];
+                }
+                else {
+                    crossGene1[k] = gene2[k];
+                    crossGene2[k] = gene1[k];
+                }
+            }
+            crossGenome1.add(crossGene1);
+            crossGenome2.add(crossGene2);
+        }
+
+        resultPopulation.add((T) individual1.createIndividual(crossGenome1));
+        resultPopulation.add((T) individual2.createIndividual(crossGenome2));
+    }
+    return resultPopulation;
+}
+
+/**
+ *  变异
+ * */
+@SuppressWarnings("unchecked")
+private List<T> mutate(List<T> currentPopulation) {
+    return currentPopulation.stream().map(
+            individual -> (T) individual.createIndividual(individual.getGenome().stream().map(gene -> {
+                boolean[] mutateGene = new boolean[gene.length];
+                if (gene.length > 0) {
+                    mutateGene[0] = gene[0];
+                    for (int i = 1; i < gene.length; i++) {
+                        mutateGene[i] = Math.random() < P_MUTATE ? (!gene[i]) : gene[i];
+                    }
+                }
+                return mutateGene;
+            }).collect(Collectors.toList()))
+    ).collect(Collectors.toList());
+}
 
     /**
      *  getter & setter
